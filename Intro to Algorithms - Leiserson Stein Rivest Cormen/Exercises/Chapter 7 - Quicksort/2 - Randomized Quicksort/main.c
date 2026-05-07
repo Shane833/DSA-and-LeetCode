78bc886ec5aa5fdb5720fef521cbe8e14559e433
Shane833 <williamsshane883@gmail.com>
2026-05-07 18:29:55 +0530

    Learning Counting Sort

--- Intro to Algorithms - Leiserson Stein Rivest Cormen/Exercises/Chapter 7 - Quicksort/2 - Randomized Quicksort/main.c
+++ Intro to Algorithms - Leiserson Stein Rivest Cormen/Exercises/Chapter 7 - Quicksort/2 - Randomized Quicksort/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h> // contains rand()
 #include <time.h>
 #include <assert.h>
+#include <stdint.h>
 
 typedef int (*Compare)(int a, int b);
 
@@ -62,62 +63,63 @@ int hoarePartition(int A[], int left, int right, Compare cmp){
     return j;
 }
 
-/* Returns an integer in the range [0, n).
- *
- * Uses rand(), and so is affected-by/affects the same seed.
- */
-int randint(int n) {
-  if ((n - 1) == RAND_MAX) {
-    return rand();
-  } else {
-    // Supporting larger values for n would requires an even more
-    // elaborate implementation that combines multiple calls to rand()
-    assert (n <= RAND_MAX);
-
-    // Chop off all of the values that would cause skew...
-    int end = RAND_MAX / n; // truncate skew
-    assert (end > 0);
-    end *= n;
-
-    // ... and ignore results from rand() that fall above that limit.
-    // (Worst case the loop condition should succeed 50% of the time,
-    // so we can expect to bail out of this loop pretty quickly.)
-    int r;
-    while ((r = rand()) >= end);
-
-    return r % n;
-  }
-}
-
 // Choosing Random numbers in a given range of numbers
 // this is exclusive random function which also takes into account
 // the end number
 unsigned random(unsigned start, unsigned end){
     //srand((time(NULL) + rand()) % RAND_MAX); // seeding with time
-    //srand((time(NULL))); // seeding with time
+    srand((time(NULL))); // seeding with time
     unsigned nums_in_range = end - start + 1;
-    nums_in_range += 1; // Adding 1 to provide exclusive range
-    printf("Nums in range : %d\n", nums_in_range - 1);
+    /* NOTE : Me adding this 1 was wrong
+    //nums_in_range += 1; // Adding 1 to provide exclusive range
+    //printf("Nums in range : %d\n", nums_in_range - 1);
+    */
     unsigned rand_num = rand() % nums_in_range;
 
     return start + rand_num; // or equivalent end - rand_num
 }
 
-int randomPartition(int A[], int left, int right, Compare cmp){
-    unsigned nums_in_range = right - left + 1;
-    nums_in_range += 1;
+/* Uniformely distributed numbers
+static uint32_t randomInRange(uint32_t a, uint32_t b) {
+    uint32_t v;
+    uint32_t range;
+    uint32_t upper, lower;
+    uint32_t mask = 0;
+
+    if (a == b) return a;
+
+    if (a > b) { upper = a; lower = b; }
+    else { upper = b; lower = a; }
+
+    range = upper - lower;
+
+    // Create a mask that's the smallest power of 2 greater than or equal to 'range'
+    // This ensures that rand() & mask has a predictable power-of-2 range
+    while (mask < range) {
+        mask = (mask << 1) | 1;
+    }
+
+    // Generate a random value using the mask and retry if it exceeds the desired range
+    while (true) {
+        v = rand() & mask;
+        if (v <= range) {
+            return lower + v;
+        }
+    }
+}
+*/
+
+int randomizedPartition(int A[], int left, int right, Compare cmp){
     unsigned i = random(left, right);
-    i += left;
-    printf("left :%d right : %d index : %d\n",left, right, i);
-    //swap(A, i, right);
-    return lomutoPartition(A, left, right, cmp);
+    swap(A, i, right);
+    return hoarePartition(A, left, right, cmp);
 }
 
-void quicksort(int A[], int left, int right, Compare cmp){
+void randomizedQuicksort(int A[], int left, int right, Compare cmp){
     if(left < right){
-        int q = randomPartition(A, left, right, cmp);
-        quicksort(A, left, q - 1, cmp);
-        quicksort(A, q + 1, right, cmp);
+        int q = randomizedPartition(A, left, right, cmp);
+        randomizedQuicksort(A, left, q - 1, cmp);
+        randomizedQuicksort(A, q + 1, right, cmp);
     }
 }
 
@@ -130,12 +132,11 @@ void display(int A[], size_t n){
 
 int main(){
     int arr[] = {2, 8, 7, 1, 3, 5, 6};
-    //int arr[] = {100, 20, 33, 469, 5090, 6};
     display(arr, sizeof(arr)/sizeof(arr[0]));
 
-    quicksort(arr, 0, sizeof(arr)/sizeof(arr[0]) - 1, (Compare)ascOrder);
+    randomizedQuicksort(arr, 0, sizeof(arr)/sizeof(arr[0]) - 1, (Compare)ascOrder);
 
     if(isSorted(arr,sizeof(arr)/sizeof(arr[0]), (Compare)ascOrder) == 0)
-    display(arr, sizeof(arr)/sizeof(arr[0]));
+        display(arr, sizeof(arr)/sizeof(arr[0]));
 
 }
